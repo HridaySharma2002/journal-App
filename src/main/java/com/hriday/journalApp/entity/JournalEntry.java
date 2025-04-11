ea42ef32e8e947b1dac57bdb61ca722345660d1b
Hriday <kshriday@gmail.com>
2025-04-11 10:44:59 +0530

    Commit Lombok changes

--- src/main/java/com/hriday/journalApp/entity/JournalEntry.java
+++ src/main/java/com/hriday/journalApp/entity/JournalEntry.java
@@ -1,6 +1,7 @@
 package com.hriday.journalApp.entity;
 
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
 
     @Id
@@ -17,37 +19,5 @@ public class JournalEntry {
     private String content;
     private LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
 
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
