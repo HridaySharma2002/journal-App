b5fa9ad2194ba7c78e09679ec498b92189017f68
Hriday <kshriday@gmail.com>
2025-04-21 06:54:15 +0530

    Add Junit for testing

---
+++ src/test/java/com/hriday/journalApp/service/UserServiceTests.java
@@ -0,0 +1,41 @@
+package com.hriday.journalApp.service;
+
+import com.hriday.journalApp.entity.User;
+import com.hriday.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class UserServiceTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Autowired
+    private UserService userService;
+
+    @Disabled
+    @ParameterizedTest
+    @ArgumentsSource(UserArgumentsProvider.class)
+    public void testSaveNewUser(User user){
+        assertTrue(userService.saveNewUser(user));
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({
+            "1,1,2",
+            "2,10,12"
+    })
+    public void test(int a,int b,int expected){
+        assertEquals(expected,a+b);
+    }
+}
